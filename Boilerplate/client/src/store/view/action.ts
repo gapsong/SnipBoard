a99fb2ade4c8697f60ac61c47a81acd80b0ba2fe
gapsong <khiemtonthat@yahoo.de>
2020-11-23 23:27:45 +0100

    added action

---
+++ Boilerplate/client/src/store/view/action.ts
@@ -0,0 +1,35 @@
+import { ActionCreator, Action, Dispatch } from 'redux';
+import { ThunkAction } from 'redux-thunk';
+import axios from 'axios';
+
+import { DashboardActionTypes } from './types';
+import { ApplicationState } from '../index';
+
+export type AppThunk = ActionCreator<
+    ThunkAction<void, ApplicationState, null, Action<string>>
+    >;
+
+export const fetchRequest: AppThunk = () => {
+    return (dispatch: Dispatch): any => {
+        try {
+            dispatch({
+                type: DashboardActionTypes.FETCH_REQUEST,
+            });
+
+            return axios({
+                method: 'GET',
+                url: "http://localhost:3001/products",
+                responseType: "json"
+            }).then((response) =>
+                dispatch({
+                    type: DashboardActionTypes.FETCH_SUCCESS,
+                    payload: response.data,
+                })
+            )
+        } catch (e) {
+            return dispatch({
+                type: DashboardActionTypes.FETCH_ERROR
+            });
+        }
+    };
+};
