e147c049a906e2e03206d480f5c9d8c800e3eacf
gapsong <khiemtonthat@yahoo.de>
2020-11-26 21:45:33 +0100

    updated readme

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,6 @@ import installExtension, {
   REDUX_DEVTOOLS,
 } from 'electron-devtools-installer';
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
-  app.quit();
-}
-
 interface Rectangle {
   height: number;
   width: number;
