2ffc0c7b29057808bf46fd632210b59c2b00aaf2
gapsong <khiemtonthat@yahoo.de>
2020-11-26 23:00:40 +0100

    add noemond

--- src/app/components/Dashboard/DraggableView.tsx
+++ src/app/components/Dashboard/DraggableView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { TextField, Button } from '@material-ui/core';
 import { Rnd } from 'react-rnd';
 import { ViewConfig } from '@types';
