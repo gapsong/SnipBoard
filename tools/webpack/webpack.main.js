44181ced3339f1a289d84b9e8eae0c3c7da6c9ee
gapsong <khiemtonthat@yahoo.de>
2020-11-29 21:12:56 +0100

    added static file copy to webpack for the viewport html

--- tools/webpack/webpack.main.js
+++ tools/webpack/webpack.main.js
@@ -1,15 +1,26 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+const rootDir = process.cwd();
+
 module.exports = {
-  /**
-   * This is the main entry point for your application, it's the first file
-   * that runs in the main process.
-   */
-  entry: ['./src/main.ts'],
-  // Put your normal webpack config below here
-  module: {
-    rules: require('./webpack.rules'),
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
-    alias: require('./webpack.aliases'),
-  },
+    /**
+     * This is the main entry point for your application, it's the first file
+     * that runs in the main process.
+     */
+    entry: ['./src/main.ts'],
+    // Put your normal webpack config below here
+    module: {
+        rules: require('./webpack.rules'),
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{ from: path.join(rootDir, 'src/static'), to: './static' }],
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
+        alias: require('./webpack.aliases'),
+    },
 };
