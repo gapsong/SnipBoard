18aa20e6e620b2e4f0dd2af62b007bb25f1dd660
gapsong <khiemtonthat@yahoo.de>
2020-12-06 23:50:30 +0100

    made it work

--- src/app/components/BrowserView/BrowserView.tsx
+++ src/app/components/BrowserView/BrowserView.tsx
@@ -1,25 +1,17 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TextField, Button } from '@material-ui/core';
-import { deleteView, dragView, onDragStart, onDragEnd } from '@src/app/store/view/action';
+import { deleteView, onDragStart, onDragEnd } from '@src/app/store/view/action';
 import { Rnd } from 'react-rnd';
-
-const preventGhostImage = (event) => {
-    //prevent ghostimage
-    const img = new Image();
-    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-    event.dataTransfer.setDragImage(img, 0, 0);
-};
 
 const BrowserView: React.FunctionComponent = () => {
     const id = window.location.search.replace('?bvid=', '');
-
     const dispatch = useDispatch();
-
     const [urlValue, setUrl] = useState('https://github.com/gapsong/');
     const [shownUrl, setShownUrl] = useState(urlValue);
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
+    const [bvStyle, setBvStyle] = useState({ x: 0, y: 0, width: '100%', height: '100%', background: 'red' });
 
     const dispatchDelete = () => {
         dispatch(deleteView(id));
@@ -35,15 +27,15 @@ const BrowserView: React.FunctionComponent = () => {
     };
 
     return (
-        // Wrapper
         <Rnd
             id='Wrapper'
-            className='appWrapper'
+            default={bvStyle}
             position={{ x, y }}
             onDragStart={(event, data) => {
                 setX(data.x);
                 setY(data.y);
                 dispatch(onDragStart(id));
+                setBvStyle({ x: 0, y: 0, width: `${data.node.clientWidth}px`, height: `${data.node.clientHeight}px`, background: 'green' });
             }}
             onDragStop={(event, data) => {
                 dispatch(
@@ -55,18 +47,16 @@ const BrowserView: React.FunctionComponent = () => {
                 );
                 setX(0);
                 setY(0);
+                setBvStyle({ x: 0, y: 0, width: '100%', height: '100%', background: 'red' });
             }}
         >
-            {/* Toolbar */}
-            <div>
+            <div style={bvStyle}>
                 <TextField
                     label='Standard'
                     type='text'
                     value={urlValue}
                     onKeyPress={(ev) => {
-                        console.log(`Pressed keyCode ${ev.key}`);
                         if (ev.key === 'Enter') {
-                            // Do code here
                             ev.preventDefault();
                             fetchUrl();
                         }
@@ -76,8 +66,8 @@ const BrowserView: React.FunctionComponent = () => {
                 <Button variant='contained' color='primary' onClick={dispatchDelete}>
                     Delete BrowserView
                 </Button>
+                <webview id='wv1' src={shownUrl}></webview>
             </div>
-            <webview className='appWrapper' id='wv1' src={shownUrl}></webview>
         </Rnd>
     );
 };
