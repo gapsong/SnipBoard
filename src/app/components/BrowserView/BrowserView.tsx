5d8e94be4a661662fc75a7fb3bc03ba3344f661f
gapsong <khiemtonthat@yahoo.de>
2020-12-07 22:44:43 +0100

    draggeable instead of rnd

--- src/app/components/BrowserView/BrowserView.tsx
+++ src/app/components/BrowserView/BrowserView.tsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { TextField, Button } from '@material-ui/core';
 import { deleteView, onDragStart, onDragEnd } from '@src/app/store/view/action';
-import { Rnd } from 'react-rnd';
+import Draggable from 'react-draggable';
 
+const initStyle = { x: 0, y: 0, width: '100%', height: '100%', background: 'red' };
 const BrowserView: React.FunctionComponent = () => {
     const id = window.location.search.replace('?bvid=', '');
     const dispatch = useDispatch();
@@ -11,7 +12,7 @@ const BrowserView: React.FunctionComponent = () => {
     const [shownUrl, setShownUrl] = useState(urlValue);
     const [x, setX] = useState(0);
     const [y, setY] = useState(0);
-    const [bvStyle, setBvStyle] = useState({ x: 0, y: 0, width: '100%', height: '100%', background: 'red' });
+    const [bvStyle, setBvStyle] = useState(initStyle);
 
     const dispatchDelete = () => {
         dispatch(deleteView(id));
@@ -27,17 +28,16 @@ const BrowserView: React.FunctionComponent = () => {
     };
 
     return (
-        <Rnd
-            id='Wrapper'
-            default={bvStyle}
+        <Draggable
+            defaultPosition={{ x: 0, y: 0 }}
             position={{ x, y }}
-            onDragStart={(event, data) => {
+            onStart={(event, data) => {
                 setX(data.x);
                 setY(data.y);
                 dispatch(onDragStart(id));
                 setBvStyle({ x: 0, y: 0, width: `${data.node.clientWidth}px`, height: `${data.node.clientHeight}px`, background: 'green' });
             }}
-            onDragStop={(event, data) => {
+            onStop={(event, data) => {
                 dispatch(
                     onDragEnd({
                         id: id,
@@ -47,7 +47,7 @@ const BrowserView: React.FunctionComponent = () => {
                 );
                 setX(0);
                 setY(0);
-                setBvStyle({ x: 0, y: 0, width: '100%', height: '100%', background: 'red' });
+                setBvStyle(initStyle);
             }}
         >
             <div style={bvStyle}>
@@ -68,7 +68,7 @@ const BrowserView: React.FunctionComponent = () => {
                 </Button>
                 <webview id='wv1' src={shownUrl}></webview>
             </div>
-        </Rnd>
+        </Draggable>
     );
 };
 
