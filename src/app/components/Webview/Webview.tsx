10ab7fd60af9e6df17e81faffbebe9048aaf10cf
Khiem Ton-That <khiem.ton-that@jameda.de>
2020-12-13 13:31:24 +0100

    added preload

--- src/app/components/Webview/Webview.tsx
+++ src/app/components/Webview/Webview.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 
 type WebviewProps = {
-    id: string;
-    url: string;
+  id: string;
+  url: string;
 };
 
 const Webview: React.FunctionComponent<WebviewProps> = ({ id, url }) => {
-    return <webview id={id} style={{ width: '100%', height: '100%', minHeight: '100px' }} src={url} preload='./preload.js' />;
+  // @ts-ignore
+  const path = window.api.MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY;
+  return (
+    <webview
+      id={id}
+      style={{ width: '100%', height: '100%', minHeight: '100px' }}
+      src={url}
+      preload={`file://${path}/preload.js`}
+    />
+  );
 };
 
 export { Webview };
---
+++ src/preload.ts
@@ -0,0 +1,61 @@
+import { contextBridge, ipcRenderer } from 'electron';
+import {
+  PONG,
+  CREATE_VIEW,
+  INIT_DASHBOARD,
+  UPDATE_VIEW_POSITION,
+  UPDATE_URL,
+} from '@src/common/channels';
+
+const validChannels = [
+  PONG,
+  UPDATE_URL,
+  INIT_DASHBOARD,
+  UPDATE_VIEW_POSITION,
+  CREATE_VIEW,
+];
+
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+contextBridge.exposeInMainWorld('api', {
+  MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: __dirname,
+  //send: (channel, data) => {
+  request: (channel: string, data: string) => {
+    // whitelist channels
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  //receive: (channel, func) => {
+  response: (channel: string, func: any) => {
+    if (validChannels.includes(channel)) {
+      // Deliberately strip event as it includes `sender`
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  },
+  //receive: (channel, func) => {
+  removeListener: (channel: string, func: any) => {
+    if (validChannels.includes(channel)) {
+      ipcRenderer.removeListener(channel, (event, ...args) => func(...args));
+    }
+  },
+  removeAllListeners: (channel: string) => {
+    if (validChannels.includes(channel)) {
+      ipcRenderer.removeAllListeners(channel);
+    }
+  },
+});
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector: string, text: string) => {
+    const element = document.getElementById(selector);
+    if (element) {
+      element.innerText = text;
+    }
+  };
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, (process.versions as any)[type]);
+  }
+});
